ad3c6b644e934b9f9ca4a4d36cbba8a161adcf81
andresmntr <andres.montero.ranc@alumnos.upm.es>
2018-05-10 18:23:49 +0200

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+
+const Sequelize = require("sequelize");
+const {models} = require("../models/index.js");
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'P5_QUIZ' });
+});
+
+router.get('/credits', (req, res, next) => {
+	res.render('credits', {title: "P5_QUIZ", name: "Manuel Cano Rueda"});
+});
+
+router.get('/quizzes', (requ, res, next) => {
+	models.quiz.findAll().then( (quizzes) => {
+		res.render('quizzes', {quizzes});
+	})
+	.catch(error => {
+	});
+});
+
+module.exports = router;
